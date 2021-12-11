f6a81eca199ef40ebde6c78ab3959c977387e676
Rolando Marin <RolandoGalvanM@outlook.com>
2021-12-11 02:18:20 -0800

    Final upload

---
+++ Marin_linkedLists/LinkedList.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marin_linkedLists
+{
+    class LinkedList
+    {
+        public Node Add(Node head, Node newNode)
+        {
+            if (head == null)
+                return newNode;
+
+
+            Node current = head;
+
+            while (current != null)
+            {
+                if (current.Next == null)
+                {
+                    current = newNode;
+                    return head;
+
+                }
+                else
+                    current = current.Next;
+
+            };
+            return head;
+
+        }
+
+        public Node Contains(Node head, string item)
+        {
+            if (head == null)
+                return null;
+            Node current = head;
+
+            while (current != null)
+            {
+                if (current.Data.ToLower() == item.ToLower())
+                    return current;
+                else
+                    current = current.Next;
+
+            };
+            return null;
+        }
+        public Node Remove(Node head, string item)
+        {
+            if (head.Data.ToLower() == item.ToLower() && head.Next == null)
+            {
+                head = null;
+                return head;
+
+            }
+            else if (head.Data.ToLower() == item.ToLower())
+            {
+                head = head.Next;
+                return head;
+            }
+            Node current = head;
+
+            while (current.Next != null)
+            {
+                if (current.Next.Data.ToLower() == item.ToLower() && current.Next.Next == null)
+                {
+                    current.Next = null;
+                    return head;
+                }
+                else if (current.Next.Data.ToLower() == item.ToLower())
+                {
+                    current.Next = current.Next.Next;
+                    return head;
+                }
+                else
+                    current = current.Next;
+            };
+            return head;
+
+        }
+
+        public string PrintNodes(Node head)
+        {
+            
+            Node current = head;
+            string allNode = "- " + current.Data;
+
+            while (current.Next != null)
+            {
+                current = current.Next;
+                allNode += $"\n- {current.Data}";
+            };
+            return allNode;
+        }
+
+
+    }
+}
--- Marin_linkedLists/Program.cs
+++ Marin_linkedLists/Program.cs
@@ -1,12 +1,109 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Threading;
+//Rolando Marin
 namespace Marin_linkedLists
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Githiub!");
+            Node head = null;
+
+            LinkedList items = new LinkedList();
+
+            string selection;
+            
+            do
+            {
+                Console.Clear();
+
+                Console.WriteLine(Menu());
+
+                selection = Console.ReadLine();
+
+                if (selection == "1") 
+                {
+                    Console.WriteLine("Create Item: ");
+                    string newNode = Console.ReadLine();
+
+                    head = items.Add(head, new Node(newNode));
+                    Console.WriteLine(newNode + " Added");
+                    Console.ReadLine();
+                    Console.WriteLine("Press Enter to return");
+                }
+                else if(selection == "2")
+                {
+                    Console.WriteLine("Enter Node we are deleting: ");
+                    string stringS = Console.ReadLine();
+
+                    Node searchNode = FindNode(head, items, stringS);
+                    
+                    if(searchNode != null)
+                    {
+                        head = items.Remove(head, stringS);
+                        Console.WriteLine(stringS + " Was removed");
+
+                        
+                    }
+                    Console.ReadLine();
+
+                }
+                else if(selection == "3")
+                {
+                    Console.WriteLine("What node are we looking for?");
+                    string stringS = Console.ReadLine();
+                    Node searchNode = FindNode(head, items, stringS);
+
+                    Console.WriteLine(stringS + " was Found");
+                    Console.ReadLine();
+                    
+                }
+                else if (selection == "4")
+                {
+                    Console.WriteLine(items.PrintNodes(head));
+                    Console.ReadLine();
+
+                }
+                else if (selection == "5")
+                {
+                    break;
+                } 
+                
+
+
+
+
+            }while(true);
+
+
+            static Node FindNode(Node head, LinkedList items, string stringS)
+            {
+                Node foundNode = items.Contains(head, stringS);
+                if (foundNode != null)
+                {
+                    return foundNode;
+                }
+                else
+                return null;
+
+            }
+                    
+            }
+
+
+
+            static string Menu()
+            {
+                return "1: Add item\n" +
+                       "2: Remove Item \n" +
+                       "3. Search  for Item\n" +
+                       "4. Print all Nodes \n" +
+                       "5. Exit \n" +
+                       "Choose One: ";
+            }
+           
+
         }
-    }
+
 }
